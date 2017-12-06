ff3120e3eccc3e7688f19895978939d8e5ed4c00
Marek Dohnal <datgaspkitteh@gmail.com>
2017-12-06 22:04:54 +0100

    21.

--- Bjarne PPP - CH 4 Ex. 19/Bjarne PPP - CH 4 Ex. 19.cpp
+++ Bjarne PPP - CH 4 Ex. 19/Bjarne PPP - CH 4 Ex. 19.cpp
@@ -4,9 +4,11 @@ int main()
 {
 	vector<string> names;
 	vector<int> scores;
+	vector<string> found_names;
 	string name = "NoName";
 	int score = 0;
 	bool was_twice = false;
+	bool found_match = false;
 
 	cout << "Please enter a name (name can not be entere twice) and an integer separated by space, to terminate input, please enter: NoName 0:\n";
 	while (cin >> name >> score)
@@ -35,16 +37,40 @@ int main()
 
 	if (was_twice == false && names.size() > 0)
 	{
-		cout << '\n' << "This is the vector output:\n";
-		for (int i = 0; i < names.size(); i++)
+		cout << '\n' << "Please enter a name for which you'd like to see a score.\n:";
+		while (cin >> score)
 		{
-			cout << names[i] << '\t' << scores[i] << '\n';
+			found_match = false;
+			found_names.clear();
+
+			for (int i = 0; i < scores.size(); i++)
+			{
+				if (score == scores[i])
+				{
+					found_names.push_back(names[i]);
+					found_match = true;
+				}
+				else if (i == names.size() - 1 && found_match == false)
+				{
+					cout << "name not found\n";
+				}
+				if (i == names.size() - 1 && found_match == true)
+				{
+					cout << "The name(s) for the score of " << score << " is/are: { ";
+						for (string x : found_names)
+						{
+							cout << x << " ";
+						}
+					cout << "}.\n";
+				}
+			}
+		
 		}
 	}
 
 	else
 	{
-		cout << "A name was entered twice, or no value was entered at all, thus no output could've been produced.\n";
+		cout << "A name was entered twice, thus no output could've been produced.\n";
 	}
 	keep_window_open();
 	
